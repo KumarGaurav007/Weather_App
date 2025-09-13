f35843caa86f311988af4013c716d04ee2b194af
KumarGaurav007 <gauravkumarsk07@gmail.com>
2025-09-13 14:43:44 +0530

    new features added

--- script.js
+++ script.js
@@ -9,26 +9,30 @@ async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
     if (response.status == 404) {
+        alert("City not found!");
         document.querySelector('.error').style.display = 'block';
         document.querySelector('.weather').style.display = 'none';
+
+        document.querySelector(".card2").style.display = "none"
+
+        return;
     }
     else {
         document.querySelector('.error').style.display = 'none';
+        document.querySelector(".card2").style.display = "block";
+
 
     }
 
     var data = await response.json();
 
-    if (data.cod === "404") {
-        alert("City not found!");
-        return;
-    }
 
+    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " m/s";
 
     if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "assets/clouds.png"
@@ -48,12 +52,53 @@ async function checkWeather(city) {
     else if (data.weather[0].main == "Snow") {
         weatherIcon.src = "assets/snow.png"
     }
+    else if (data.weather[0].main == "Haze") {
+        weatherIcon.src = "assets/haze.png"
+    }
+    else {
+        weatherIcon.src = "assets/rain.png"; // fallback
+    }
+
 
     document.querySelector('.weather').style.display = 'block'
 
+    document.querySelector(".sunrise").innerHTML = convertTime(data.sys.sunrise, data.timezone);
+    document.querySelector(".sunset").innerHTML = convertTime(data.sys.sunset, data.timezone);
+    document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa";
+
+    let visibilityInKm = (data.visibility / 1000).toFixed(1) + " km";
+    document.querySelector(".visibility").innerHTML = visibilityInKm;
+
 }
 
-searchBtn.addEventListener("click", () => {
-    checkWeather(searchBox.value);
+const infoBtn = document.querySelector(".info-btn");
+infoBtn.addEventListener("click", () => {
+    document.querySelectorAll(".details2").forEach(el => {
+        el.style.display = "flex";
+    });
 
-})
+});
+
+searchBtn.addEventListener("click", () => {
+    if (searchBox.value == '') {
+        alert("Enter City name");
+        document.querySelector('.error').style.display = 'block';
+        return;
+    }
+    else {
+        checkWeather(searchBox.value);
+        document.querySelectorAll(".details2").forEach(el => {
+            el.style.display = "none";
+        });
+    }
+});
+
+function convertTime(timestamp, timezone) {
+    let localTime = new Date((timestamp + timezone) * 1000);
+
+    let hours = localTime.getUTCHours().toString().padStart(2, "0");
+    let minutes = localTime.getUTCMinutes().toString().padStart(2, "0");
+    let seconds = localTime.getUTCSeconds().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+}
